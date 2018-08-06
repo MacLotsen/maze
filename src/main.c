c229004a402866aa886a99f448a339d6d71f8cb8
Erik Nijenhuis <erik@judas.io>
2018-08-06 19:07:51 +0200

    Add options and add library

--- src/main.c
+++ src/main.c
@@ -6,31 +6,54 @@
 #include <stdlib.h>
 #include <time.h>
 #include <locale.h>
+#include <getopt.h>
 
+#include "maze.h"
 #include "maze-renderer.h"
 
-#define WIDTH 15
-#define HEIGHT 7
-
 int main(int argc, char **argv) {
+    int flag_simple = 0;
+    int value_width = 15;
+    int value_height = 7;
+    int c;
+
+    opterr = 0;
+
+    while((c = getopt(argc, argv, "sw:h:")) != -1)
+      switch(c) {
+        case 's':
+          flag_simple = 1;
+          break;
+        case 'w':
+          value_width = atoi(optarg);
+          break;
+        case 'h':
+          value_height = atoi(optarg);
+          break;
+        default:
+          return 1;
+        }
+
     srand((unsigned int) time(0));
 
     setlocale(LC_ALL, "");
 
     // create maze
-    maze_t *maze = create_maze(WIDTH, HEIGHT);
+    maze_t *maze = create_maze(value_width, value_height);
 
     // open top-right corner
-    maze->tiles[WIDTH-1] |= TOP_OPEN;
+    maze->tiles[value_width-1] |= TOP_OPEN;
     // open bottom-left corner
-    maze->tiles[(HEIGHT - 1) * WIDTH] |= BOTTOM_OPEN;
+    maze->tiles[(value_height - 1) * value_width] |= BOTTOM_OPEN;
     // open top-mid
 //    maze->tiles[WIDTH / 2] |= TOP_OPEN;
     // open bottom-mid
 //    maze->tiles[(HEIGHT - 1) * WIDTH + (WIDTH / 2)] |= BOTTOM_OPEN;
 
-    //simple_renderer(maze);
-    box_drawing_renderer(maze);
+    if (flag_simple)
+      simple_renderer(maze);
+    else
+      box_drawing_renderer(maze);
 
     destroy_maze(maze);
 }
